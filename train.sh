3a6a436f2f93e21856cdd2f0f64b2bae22b2881a
congxu96 <125612389+congxu96@users.noreply.github.com>
2023-12-18 17:25:57 -0600

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,5 @@
+python time_series_forecasting/training.py --data_csv_path "data/processed_data.csv" \
+                                           --feature_target_names_path "data/config.json" \
+                                           --output_json_path "models/trained_config.json" \
+                                           --log_dir "models/ts_views_logs" \
+                                           --model_dir "models/ts_views_models"
